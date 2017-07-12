f11ff70e1d31999821f131283938e75516f271ff
Suhas V <suvihas@gmail.com>
2017-07-12 13:08:59 +0530

    add error handling

--- action.js
+++ action.js
@@ -6,38 +6,55 @@ var bodyParser = require('body-parser');
 var filter = function() {
     return function(req, res, next) {
 
-        console.log("Inside function: filter");
+        if(!req.body.payload) {
+        	console.log("Error! Payload missing!");
+        	return res.status(400).json({
+                    'error': 'Could not decode request: JSON parsing failed'
+                });
+        }
 
         var payloadArray = req.body.payload;
+
+        if(payloadArray.length === 0) {
+        	console.log("Error! Empty payload!");
+        	return res.status(400).json({
+                    'error': 'Empty payload'
+                });
+        }
 
         getResultSet(payloadArray, [], function(err, resultArray) {
             if (err) {
                 console.log("Error: " + util.inspect(err));
-                return res.status(500).json({
-                    'msg': 'Error '
+                return res.status(400).json({
+                    'error': 'Could not decode request: JSON parsing failed'
                 });
             }
             res.status(200).json({
-                data: resultArray
+                response: resultArray
             });
         });
     }
 }
 
 var getResultSet = function(payload, returnArray, cb) {
+    //exit point for recursive call
     if (payload.length === 0) {
         return cb(null, returnArray);
     }
     var item = payload.pop();
+    
+    if (!item.type || !item.workflow) {
+        console.log("Error! Item type or workflow missing!");
+        return cb(new Error("type or workflow missing in payload"));
+    }
 
     if ((item.type == "htv") && (item.workflow == "completed")) {
-
         //this is a wanted record, construct result object and push to result set
-        var building = item.address.buildingNumber;
-        var street = item.address.street;
-        var suburb = item.address.suburb;
-        var state = item.address.state;
-        var pincode = item.address.postcode;
+        var building = item.address.buildingNumber || '';
+        var street = item.address.street || '';
+        var suburb = item.address.suburb || '';
+        var state = item.address.state || '';
+        var pincode = item.address.postcode || '';
 
         var address = [building, street, suburb, state, pincode];
 
@@ -47,8 +64,8 @@ var getResultSet = function(payload, returnArray, cb) {
         data.workflow = item.workflow;
         returnArray.push(data);
     }
+    //recursive call to process payload
     getResultSet(payload, returnArray, cb);
-
 }
 
 
